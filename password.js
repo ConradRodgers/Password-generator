77cbeebaec8e1d293fd15209889ce66938f201ff
Conrad Rodgers <conrad13500@gmail.com>
2020-01-14 19:47:33 -0800

    first draft

---
+++ password.js
@@ -0,0 +1,46 @@
+//Generate random password
+function generate() {
+  //sets password length and complexity
+  let complexity = document.getElementById("slider").value;
+
+  //password values
+  let values =
+    "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890!@!#$%^&*()";
+  let password = "";
+
+  // for loop for chooing chars have question
+  for (var i = 0; i <= complexity; i++) {
+    password =
+      password +
+      values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  // insert password to display
+
+  document.getElementById("display").value = password;
+
+  //list out old passwords
+  document.getElementById("lastNums").innerHTML += password + "<br />";
+}
+
+// default slider setting
+document.getElementById("length").innerHTML = "Length: 8";
+
+//slider readout display
+
+document.getElementById("slider").oninput = function() {
+  if (document.getElementById("slider").value > 0) {
+    document.getElementById("length").innerHTML =
+      "Length:" + document.getElementById("slider").value;
+  } else {
+    document.getElementById("length").innerHTML = "Length: 1";
+  }
+};
+
+function copyPassword() {
+  document.getElementById("display").select();
+
+  document.execCommand("Copy");
+
+  alert("Your Password has been copied");
+}
